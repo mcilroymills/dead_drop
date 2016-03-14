b8b35821c782666e074eeddc6c318769167473e5
millsmcilroy <millsmcilroy@gmail.com>
2016-03-14 15:41:56 -0600

    got rid of keye

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -9,14 +9,16 @@
       zoom: 13
     });
     for (var i = 0; i < pins.length; i++) {
-
+      var latitude = parseFloat(pins[i].latitude);
+      var longitude = parseFloat(pins[i].longitude);
 
       var marker = new google.maps.Marker({
-        position: {lat: parseFloat(pins[i].latitude), lng:parseFloat(pins[i].longitude)},
+        position: {lat: latitude, lng:longitude},
         map: map,
         title: 'Hello World!'
       });
-      console.log(marker.position);
+      console.log(latitude);
+      console.log(longitude);
     }
   }
 
