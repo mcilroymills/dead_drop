fe11dfe550f9ab80534d4c954ebb877f37a09e1a
millsmcilroy <millsmcilroy@gmail.com>
2016-03-14 20:14:32 -0600

    cleaned up initMap code, added css to remove goog logo

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,46 +1,73 @@
 
-
-
-
-
 function initMap(pins) {
-  pins = JSON.parse(pins);
-  var markerArray = [];
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 39.739209, lng: -104.990255},
-    zoom: 13
-  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
 
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
+  pins = JSON.parse(pins);
+
+  //Declare map variables
+  var map;
+  var marker;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+    //Current position
     var pos = {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     };
 
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    map.setCenter(pos);
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
-  });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
-}
-  for (var i = 0; i < pins.length; i++) {
-    var latitude = parseFloat(pins[i].latitude);
-    var longitude = parseFloat(pins[i].longitude);
-
-    var marker = new google.maps.Marker({
-      position: {lat: latitude, lng:longitude},
-      map: map,
-      title: 'Hello World!'
+    //Create google map object
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pos,
+      zoom: 13
     });
-    console.log(latitude);
-    console.log(longitude);
+
+    //Populate map with pins from database
+    for (var i = 0; i < pins.length; i++) {
+      var latitude = parseFloat(pins[i].latitude);
+      var longitude = parseFloat(pins[i].longitude);
+
+      marker = new google.maps.Marker({
+        position: {lat: latitude, lng:longitude},
+        map: map,
+      });
+    }
+    //Error function
+  },function() {
+      //Client declined to allow current location, center map at enter map at city & county bldg
+      map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 39.739209, lng: -104.990255},
+      zoom: 13
+      });
+
+      //Populate map with pins from database
+      for (var i = 0; i < pins.length; i++) {
+        var latitude = parseFloat(pins[i].latitude);
+        var longitude = parseFloat(pins[i].longitude);
+
+        marker = new google.maps.Marker({
+          position: {lat: latitude, lng:longitude},
+          map: map,
+        });
+      }
+    });
+  } else {
+    // Browser doesn't support Geolocation, center map at city & county bldg
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.739209, lng: -104.990255},
+    zoom: 13
+    });
+    //Populate map with pins from database
+    for (var i = 0; i < pins.length; i++) {
+      var latitude = parseFloat(pins[i].latitude);
+      var longitude = parseFloat(pins[i].longitude);
+
+      marker = new google.maps.Marker({
+        position: {lat: latitude, lng:longitude},
+        map: map,
+      });
+    }
   }
 }
 
