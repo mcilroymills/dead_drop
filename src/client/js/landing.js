a645e8b5c33e444cda8a7f5ae938ee69b8ded723
Jon Hammond <jonh1016@gmail.com>
2016-03-17 14:19:03 -0600

    Fixed sessions, updated html, and edited the view for newaccount. Also added the ensureAuth helper function back into routes

--- src/client/js/landing.js
+++ src/client/js/landing.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  checkForLogout();
   hideLogo();
   hideAndShow();
 });
@@ -9,4 +10,12 @@ function hideAndShow () {
 
 function hideLogo () {
   $('#deadDropLogo').delay(2000).hide(1500);
+}
+
+function checkForLogout () {
+  if($('.logoutMessage').text()) {
+    $('#deadDropLogo').hide();
+    $('.logoutMessage').delay(2000).hide(1500);
+    $('#loginForm').hide().delay(3000).show(1500);
+  }
 }
--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -4,7 +4,9 @@ var passport = require('../db/lib/auth');
 var helpers = require('../db/lib/helper');
 
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Log In' });
+  var message = req.flash('message');
+  var logout = req.flash('logout');
+  res.render('login', { title: 'Log In', messages: message, logout: logout });
 });
 
 //route to post login and authenticate with passport when the user clicks submit on the /login page
